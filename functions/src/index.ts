94024196e5b8771a28d3a616a844c2a90194ccbe
fumiya yono <z2ab8q3ty0@gmail.com>
2021-03-04 03:06:07 +0900

    [add]stripeで課金する関数の作成

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,32 @@
-import * as functions from "firebase-functions";
+import * as express from "express";
+const functions = require("firebase-functions");
+const stripe = require("stripe")(
+  "sk_test_51IPI4WHpJiHwmrzfQNvOCQXPypFUBUdVShSws1aBfKLinI2pZKaGrAlZdQmvwRHIH5HwD1J20WcZw6eOBmO6zJ5h00Mtt61ow9"
+);
+const cors = require("cors");
+const app = express();
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+app.use(require("body-parser").text());
+app.use(cors());
+
+app.post("/charge", async (req, res) => {
+  try {
+    console.log("charge start");
+
+    let { status } = await stripe.charges.create({
+      amount: 2000,
+      currency: "jpy",
+      description: "An example charge",
+      source: req.body,
+    });
+
+    res.json({ status });
+    console.log("charge 正常終了");
+  } catch (err) {
+    console.log("charge 異常終了", err);
+    res.status(500).end();
+  }
+});
+
+const api = functions.https.onRequest(app);
+module.exports = { api };
